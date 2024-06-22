8ac7ba46227c8b01ffc3a49526768cceb1db2bfd
mfidjel <mahfoud.fidjel@gmail.com>
2024-06-22 16:24:29 +0200

    feat(backend: express-application-server)

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,11 @@
-//app.js (to be implemented)
+const express = require("express");
+const routes = require("./routes");
+
+const app = express();
+const port = 3001;
+
+app.use("/upload", routes);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
